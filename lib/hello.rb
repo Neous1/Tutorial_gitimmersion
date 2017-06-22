e24d89bf50780fede92174db6bd1e897de85d2b8
Neous1 <Neous1@users.noreply.github.com>
2017-06-21 23:08:58 -0400

    made changes to hello.rg

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,14 @@
 #Default is "World"
 #Author is Yvon J N'Chonon (yvon@email.com)
-name = #{ARGV.first}!" || "World"
+
+
+# [{
+# "key": "ctrl+`",        "command": "workbench.action.terminal.focus",
+#                         "when": "!terminalFocus"},
+# {
+# "key": "ctrl+`",        "command": "workbench.action.focusActiveEditorGroup",
+#                         "when": "terminalFocus"}]
+
+ name = ARGV.first || "World"
+
 puts "Hello, #{name}!"
